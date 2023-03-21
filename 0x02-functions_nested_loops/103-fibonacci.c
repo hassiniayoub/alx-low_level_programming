3421a7df02952b18be088e68c6848c33132bf2da
hassiniayoub <azeayoub76@gmail.com>
2023-03-21 07:51:50 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of Even Fibonacci numbers
+ * Less then 4000000
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	long a = 1, b = 2, sum = b;
+
+	while (b + a < 4000000)
+	{
+	b += a;
+	if (b % 2 == 0)
+	sum += b;
+	a = b - a;
+	++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
