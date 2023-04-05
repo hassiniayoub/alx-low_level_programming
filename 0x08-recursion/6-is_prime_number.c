0f12f009e7342ac45ef9e42424190918a29b5725
hassiniayoub <azeayoub76@gmail.com>
2023-04-05 13:19:50 -0700

    0x08. C - Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,47 +1,36 @@
 #include "main.h"
 
 /**
- * compare - compare head and tail indices for match
- * @head: index starting from left of string
- * @tail: index starting from right of string, moving backwards
- * Return: 1 if palindrome, 0 if not
+ * is_prime - recursively divide by higher divisor, skip even nums
+ * @n: number to check if prime
+ * @divisor: divisor
+ * Return: 1 if prime, 0 if not, or recursive function call
  */
 
-int compare(char *head, char *tail)
+int is_prime(int n, int divisor)
 {
-
-	if (head >= tail)
+	if (n == divisor)
 		return (1);
-	if (*head == *tail)
-		return (compare(head + 1, tail - 1));
-
-	return (0);
-}
-
-/**
- * _strlen - find length of string to access last index
- * @s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-
-	if (*s == '\0')
+	if (n % divisor == 0)
 		return (0);
-	s++;
-	return (1 + (_strlen(s)));
+	return (is_prime(n, divisor + 1));
+
 }
 
 /**
- * is_palindrome - check if palindrome
- * @s: string to check
- * Return: 1 if palindrome, 0 if not
+ * is_prime_number - check if prime
+ * @n: number to check
+ * Return: 1 if prime, 0 if not
  */
 
-int is_palindrome(char *s)
+int is_prime_number(int n)
 {
-	int len = _strlen(s);
+	int divisor = 3;
 
-	return (compare(s, (s + len - 1)));
+	if (n % 2 == 0 || n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+
+	return (is_prime(n, divisor));
 }
