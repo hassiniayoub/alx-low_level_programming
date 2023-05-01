5012bbd3697857dc770192620164ea9382fa427f
hassiniayoub <azeayoub76@gmail.com>
2023-05-01 08:52:33 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,7 +8,8 @@ void free_listint(listint_t *head)
 {
 	listint_t *current_node;
 
-	while (head != NULL) { /* iterate through list until end */
+	while (head != NULL) /* iterate through list until end */
+	{
 		current_node = head; /* set current node to head */
 		head = head->next; /* move head to next node */
 		free(current_node); /* free current node */
