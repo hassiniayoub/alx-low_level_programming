cc2c927d0467e41adf5c8ae0300445a19a8c04bd
hassiniayoub <azeayoub76@gmail.com>
2023-05-01 08:45:17 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list.
+ * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: pointer to head pointer of linked list
  * @n: data to add to new node
  * Return: address of new element, or NULL if failed
@@ -9,25 +9,25 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new_node, *last_node;
+	listint_t *new_node, *last_node;
 
-    new_node = malloc(sizeof(listint_t)); /* create new node and set values */
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(listint_t)); /* new node and set values */
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
-    if (*head == NULL) /* account for empty list by pointing head to node */
-        *head = new_node;
-    else
-    {
-        last_node = *head; /* set tmp ptr to iterate w/o moving head ptr */
+	if (*head == NULL) /* account for empty list, pointing head to node */
+		*head = new_node;
+	else
+	{
+		last_node = *head; /* set tmp ptr to w/o moving head ptr */
 
-        while (last_node->next != NULL) /* iterate till last node */
-            last_node = last_node->next;
-        last_node->next = new_node; /* link last node to new node */
-    }
+		while (last_node->next != NULL) /* iterate till last node */
+			last_node = last_node->next;
+		last_node->next = new_node; /* link last node to new node */
+	}
 
-    return (new_node);
+	return (new_node);
 }
