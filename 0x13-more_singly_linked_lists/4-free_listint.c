bdfa8230314db02bc6ab34911cc68d75e2645d0e
hassiniayoub <azeayoub76@gmail.com>
2023-05-01 08:51:13 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: pointer to the head of the list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current_node;
+
+	while (head != NULL) { /* iterate through list until end */
+		current_node = head; /* set current node to head */
+		head = head->next; /* move head to next node */
+		free(current_node); /* free current node */
+	}
+
+	if (head == NULL) /* account for empty linked list */
+		return;
+}
